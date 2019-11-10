747d51754b62933330901abe97ecdcb852525b98
Aratil <56776378+Aratil@users.noreply.github.com>
2019-11-10 16:24:09 -0500

    * Plant1.sln: commit
    * Startup.cs:
    * Program.cs:
    * appsettings.json:
    * Error.cshtml:
    * Index.cshtml:
    * site.js:
    * Privacy.cshtml:
    * site.css:
    * Error.cshtml.cs:
    * Index.cshtml.cs:
    * _ViewStart.cshtml:
    * Privacy.cshtml.cs:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * launchSettings.json:
    * LICENSE.txt:
    * jquery.js:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap-reboot.css:
    * LICENSE.txt:
    * jquery.validate.js:
    * additional-methods.js:
    * jquery.validate.unobtrusive.js:

--- Plant1/Program.cs
+++ Plant1/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Plant1
+namespace plant1
 {
     public class Program
     {
