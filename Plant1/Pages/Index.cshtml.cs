0f19fb7b78afc9fc0215a140361dca3b4b584d57
Aratil <56776378+Aratil@users.noreply.github.com>
2019-11-10 17:46:45 -0500

    shoe plants and html

--- Plant1/Pages/Index.cshtml.cs
+++ Plant1/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -23,6 +24,21 @@ namespace plant1.Pages
             int age = 31;
             ViewData["MyName"] = myName;
             ViewData["age"] = age;
+
+            //download JSON data.
+            //a webClient giv es us access to data on internet
+
+            using(WebClient webClient = new WebClient())
+            {
+                string jsonData = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
+                QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonData);
+                List<QuickType.Specimen> allSpecimens = welcome.Specimens;
+                ViewData["allSpecimens"] = allSpecimens;
+                foreach (QuickType.Specimen specimen in allSpecimens)
+                {
+                    Console.WriteLine(specimen);
+                }
+            }
         }
     }
 }
