6c9bd06e1bd4fa3ff4c78a8c835d2677f7312d0f
Aratil <56776378+Aratil@users.noreply.github.com>
2019-11-10 19:43:38 -0500

    weather data

--- Plant1/Pages/Index.cshtml.cs
+++ Plant1/Pages/Index.cshtml.cs
@@ -24,18 +24,31 @@ namespace plant1.Pages
             int age = 31;
             ViewData["MyName"] = myName;
             ViewData["age"] = age;
+            long precip = 0;
+            IList<QuickType.Specimen> waterLovingSpecimens = new List<QuickType.Specimen>();
 
             //download JSON data.
             //a webClient giv es us access to data on internet
 
-            using(WebClient webClient = new WebClient())
+            using (WebClient webClient = new WebClient())
             {
-                string wetherData = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=b1326b6a9b2e40c7a068708325bc268c");
+                string weatherAPIkey = System.IO.File.ReadAllText("wetherAPI.txt");
+                string weatherData = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + weatherAPIkey);
+
+
+                QuickTypeWeather.Weather weather = QuickTypeWeather.Weather.FromJson(weatherData);
+                QuickTypeWeather.Datum[] allWeatherData = weather.Data;
+
+                foreach (QuickTypeWeather.Datum datum in allWeatherData)
+                {
+                    precip = datum.Precip;
+                }
+
                 string plantData = webClient.DownloadString("http://www.plantplaces.com/perl/mobile/viewplantsjsonarray.pl?WetTolerant=on");
                 QuickTypePlant.Plant[] allPlants = QuickTypePlant.Plant.FromJson(plantData);
 
                 IDictionary<long, QuickTypePlant.Plant> plantDictionary = new Dictionary<long, QuickTypePlant.Plant>();
-                foreach(QuickTypePlant.Plant plant in allPlants)
+                foreach (QuickTypePlant.Plant plant in allPlants)
                 {
                     plantDictionary.Add(plant.Id, plant);
                 }
@@ -44,23 +57,36 @@ namespace plant1.Pages
                 QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonData);
                 List<QuickType.Specimen> allSpecimens = welcome.Specimens;
 
-                
-                IList<QuickType.Specimen> waterLovingSpecimens = new List<QuickType.Specimen>();
+
+
 
                 foreach (QuickType.Specimen specimen in allSpecimens)
                 {
                     Console.WriteLine(specimen);
-                    if (plantDictionary.ContainsKey(specimen.PlantId) )
+                    if (plantDictionary.ContainsKey(specimen.PlantId))
                     {
                         waterLovingSpecimens.Add(specimen);
 
                     }
-                   
-                }
-                ViewData["allSpecimens"] = waterLovingSpecimens;
 
+                }
+
+
+
+                if (precip < 1)
+                {
+                    ViewData["WeatherMessage"] = "Not much precip; water these water-loving plants.";
+                    // make the specimen data available to our web page.
+                    ViewData["allSpecimens"] = waterLovingSpecimens;
+                }
+                else
+                {
+                    ViewData["WeatherMessage"] = "Lots of rain, these plants will love it!";
+                    ViewData["allSpecimens"] = waterLovingSpecimens;
+                }
             }
-           
+         
         }
+        
     }
 }
